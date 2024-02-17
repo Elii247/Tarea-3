ef1ae63adfaf55eb9bd54aa790dcd50dbd6efd9f
Prog- <elizardo.powell@uip.edu.pa>
2024-02-16 19:05:03 -0500

    Tarea 3

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean visitante = false, empleado = false;
-        byte edad = 90, cantidad = 10;
-        int altura = 68, apartamento = 200, peso = 400;
+        byte edad = 90;
+        int altura = 68, apartamento = 200, peso = 400, cantidad = 10;
         long telefono = 1823638932;
         String nombre = "10" ;
         char genero = '2';
@@ -15,34 +15,34 @@ public class Main {
       //Se ingresa en teclado
 
         System.out.println("El visitante es un Empleado (Si/No): ");
-        scanner.nextBoolean();
+        empleado= scanner.nextBoolean();
 
         System.out.println("\nEs un visitante nuevo (Si/No): ");
-        scanner.nextBoolean();
+        visitante = scanner.nextBoolean();
 
         System.out.println("\nNombre: ");
-        scanner.next();
+        nombre = scanner.next();
 
         System.out.println("\nEdad: ");
-        scanner.nextByte();
+        edad = scanner.nextByte();
 
         System.out.println("\nGenero: ");
-        scanner.next().charAt(0);
+        genero = scanner.next().charAt(0);
 
         System.out.println("\nPeso del visitante (En Kg): ");
-        scanner.nextShort();
+        peso = scanner.nextShort();
 
         System.out.println("\nAltura del visitante (cm): ");
-        scanner.nextInt();
+        altura = scanner.nextInt();
 
         System.out.println("\nNumero de telefono: ");
-        scanner.nextLong();
+        telefono = scanner.nextLong();
 
         System.out.println("\nNumero de apartamento: ");
-        scanner.nextInt();
+        apartamento = scanner.nextInt();
 
         System.out.println("\nCantidad de visitantes: ");
-        scanner.nextInt();
+        cantidad = scanner.nextInt();
 
        //Se imprime en pantalla
         System.out.println("El visitante es un Empleado: " + empleado);
@@ -50,7 +50,8 @@ public class Main {
         System.out.println("\nNombre: " + nombre);
         System.out.println("\nEdad: " + edad);
         System.out.println("\nGenero: " + genero);
-        System.out.println("\nPeso del visitante: " + visitante);
+        System.out.println("\nPeso del visitante: " + peso);
+        System.out.println("\nAltura del visitante: " + altura);
         System.out.println("\nNumero de telefono: " + telefono);
         System.out.println("\nNumero de apartamento: " + apartamento);
         System.out.println("\nCantidad de visitantes: " + cantidad);
